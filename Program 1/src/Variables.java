9b07212b12169ad7010cf1746f67af3a3636f11e
liammarkey <liammarkey@192.168.1.26>
2020-09-03 14:08:41 -0400

    First Commit attempt 2

--- Program 1/src/Variables.java
+++ Program 1/src/Variables.java
@@ -4,6 +4,30 @@ public class Variables {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		byte age;
+		int gradeValue = 94;
+		char grade = 'A';
+		String firstName = "Liam";
+		String lastName = "Markey";
+		
+		
+		gradeValue = 96;
+		gradeValue = gradeValue + 2;
+		//gradeValue = 96 + 2 or 98
+		
+		age = 18;
+		
+		System.out.println("Grade: " + gradeValue);
+		System.out.println("My name is: " + firstName + " " + lastName);
+		
+		//System.out.println("My age is: " + "age"); If quotes are used, text in quotations is printed as words not variable value
+		System.out.println("My age is: " +  age);
+		//Printing a menu to the output
+		System.out.print("Menu\n");
+		System.out.print("\tMilk\n");
+		System.out.print("\tCookies\n");
+		System.out.print("\tChurros");
+		
 	}
 
 }
